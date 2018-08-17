64ca9586748414fd2da9983ef4ac98a4a4734594
Andrew Ogburn <aogburn@zoll.com>
2018-08-17 14:12:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
-apt-get update
-apt-get upgrade
+# Setup SSH keys
+# Clone this repository
 
-apt-get install curl zsh ruby-full powerline python-pip
+apt update
+apt upgrade
+
+sudo apt install curl zsh ruby-full powerline python-pip
 curl https://releases.hyper.is/download/deb
 
 # Set zsh as default shell
-chsh -s /bin/zsh $USER
+chsh -s /usr/bin/zsh aogburn
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-gconftool --type string --set /desktop/gnome/applications/terminal/exec hyper
+# gconftool --type string --set /desktop/gnome/applications/terminal/exec hyper
+
+ln -s ~/repos/dotfiles/.hyper.js ~/.hyper.js
+ln -s ~/repos/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
 
 rm .zshrc
-mv .zshrc.pre-oh-my-zsh .zshrc
+ln -s ~/repos/dotfiles/.zshrc ~/.zshrc
+# mv .zshrc.pre-oh-my-zsh .zshrc
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-gem install colorls
+sudo gem install colorls
 
 sudo -H pip install powerline-status
-
-
