bdd748b49b1cf78c754e832a75cfcddf8a4a62dd
Andrew Ogburn <andrewogburn@gmail.com>
2018-12-15 21:21:26 -0500

    feat: Install node.js from snap

--- install.sh
+++ install.sh
@@ -49,5 +49,4 @@ dconf write /org/gnome/shell/extensions/panel-date-format/format "'%a  %b  %d   
 pip install awscli --upgrade --user
 
 # Install node.js
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo snap install node --classic --channel=8
